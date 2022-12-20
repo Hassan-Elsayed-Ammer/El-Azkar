ef3ff8f0915b5f176b02f6115fe0e392862a8af4
HassanAlex <Hassan.elsayed.ammer@gmail.com>
2022-12-20 22:22:55 +0200

    El Azkar Islamic Android Application made by Code Station الللهم علمنا ما ينفعا وأنفعنا بما علمتنا أنك أنت العليم الحكيم

---
+++ app/src/main/java/com/codestation/elazkar/MainActivityViewModel.kt
@@ -0,0 +1,37 @@
+package com.codestation.elazkar
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+
+    //1- counter mutable liveData
+    private val _counterList:MutableLiveData<Int> = MutableLiveData<Int>()
+
+    //2- counter LiveData
+    val counterList:LiveData<Int> = _counterList
+
+    var counter = 0
+
+
+    //add data in mutable LiveData
+    fun addNumbersInCounterList(){
+        counter++
+        _counterList.value = counter
+    }
+
+
+
+
+
+
+
+
+
+    // observer from live data
+
+    // sigin table live data to live data
+}
