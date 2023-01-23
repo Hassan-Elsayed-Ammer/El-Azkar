fe4445b960dcf9a0e5856013566690883951f077
HassanAlex <Hassan.elsayed.ammer@gmail.com>
2023-01-23 14:11:59 +0200

    -Add assets folder

--- app/src/main/java/com/codestation/elazkar/ui/fragments/MorningAzkarFragment.kt
+++ app/src/main/java/com/codestation/elazkar/ui/fragments/MorningAzkarFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.codestation.elazkar.R
+import com.codestation.elazkar.adapters.ElzkerAdapter
+import com.codestation.elazkar.data.ElZekr
+import org.json.JSONObject
 
 
 class MorningAzkarFragment : Fragment() {
+
+    private lateinit var adapter: ElzkerAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var elzekerList :ArrayList<ElZekr>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +25,16 @@ class MorningAzkarFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_morning_azkar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView  = view.findViewById(R.id.recycler_morning)
+        adapter = ElzkerAdapter()
+        recyclerView.adapter = adapter
+        val jsonObject = JSONObject()
+
+
     }
 
 }
