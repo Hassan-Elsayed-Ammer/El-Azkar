303265be990dba49c8dc9a02bd86fd82069c0bb6
HassanAlex <Hassan.elsayed.ammer@gmail.com>
2023-02-02 18:35:11 +0200

    -Enhancement

--- app/src/main/java/com/codestation/elazkar/ui/MainActivity.kt
+++ app/src/main/java/com/codestation/elazkar/ui/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.codestation.elazkar.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.codestation.elazkar.R
 import com.codestation.elazkar.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
 
     // create nav controller to use navigation in our main activity
     private lateinit var navController: NavController
@@ -29,33 +31,41 @@ class MainActivity : AppCompatActivity() {
         val toolBar = binding.toolbar
         val bottomNavigation = binding.bottomNavigation
 
-
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.morningAzkarFragment,
+                R.id.eveningAzkarFragment,
+                R.id.elsabhaFragment
+            )
+        )
 
         // create reference from nave host fragment
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         // reassign the navController to nav host because nav host has already nav Controller to control on fragments that will appeared in feature
         navController = navHostFragment.findNavController()
+
 
         // set Action bar
         setSupportActionBar(toolBar)
 
+//        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+//        supportActionBar?.setHomeButtonEnabled(false)
+
+        //make link between action bar and navController
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
         //set up bottom navigation view
         bottomNavigation.setupWithNavController(navController)
+
+
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-//    /**
-//     * function to increase counter  */
-//    private fun increase() {
-//        counter++
-//        tvCounter.text = counter.toString()
-//        btnCounter.text = counter.toString()
-//    }
-//
 
 
 }
